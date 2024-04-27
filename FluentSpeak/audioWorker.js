3f477ed664d2609e6e4e214d4a0ad221a902ccfd
terryking0711 <128213469+terryking0711@users.noreply.github.com>
2024-04-27 11:37:23 +0800

    Update audioWorker.js

--- FluentSpeak/audioWorker.js
+++ FluentSpeak/audioWorker.js
@@ -1,1 +1,9 @@
+importScripts('lamejs.min.js');
+
+self.onmessage = function(e) {
+    const audioData = new Int16Array(e.data);
+    const mp3encoder = new lamejs.Mp3Encoder(1, 44100, 128); // mono 44.1kHz 128kbps
+    const mp3Data = mp3encoder.encodeBuffer(audioData); // 將 pcm 資料轉換為 mp3
+    self.postMessage(mp3Data);
+};
 
